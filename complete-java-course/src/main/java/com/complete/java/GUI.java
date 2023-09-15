151aa1ba82450f98162faf01e75cfe3c4ba3c6a5
Billy Torres <btorresr75@gmail.com>
2023-09-14 19:59:12 -0600

    GUI in java

---
+++ complete-java-course/src/main/java/com/complete/java/GUI.java
@@ -0,0 +1,13 @@
+package com.complete.java;
+
+import javax.swing.JFrame;
+
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+
+    }
+}
