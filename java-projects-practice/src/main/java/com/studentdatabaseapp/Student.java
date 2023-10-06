fb47b228ee7c41da98546f724fd66a46c27be0c1
Billy Torres <btorresr75@gmail.com>
2023-10-05 19:57:08 -0600

    refactor(Student) : refactoring code

--- java-projects-practice/src/main/java/com/studentdatabaseapp/Student.java
+++ java-projects-practice/src/main/java/com/studentdatabaseapp/Student.java
@@ -48,6 +48,19 @@ public class Student {
         } while (1 != 0);
 
         System.out.println("ENROLLED IN: " + courses);
-        System.out.println("TUITION BALANCE: " + tuitionBalance);
+    }
+
+    public void viewBalance() {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
+
+    public void payTuition() {
+        viewBalance();
+        System.out.println("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
     }
 }
--- java-projects-practice/src/main/java/com/studentdatabaseapp/StudentDatabaseApp.java
+++ java-projects-practice/src/main/java/com/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,5 +3,7 @@ package com.studentdatabaseapp;
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         Student student1 = new Student();
+        student1.enroll();
+        student1.payTuition();
     }
 }
