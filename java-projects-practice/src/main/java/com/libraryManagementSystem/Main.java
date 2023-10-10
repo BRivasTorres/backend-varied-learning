de7e98f5922bb645bd066597bb93d7fb407afbb5
Billy Torres <btorresr75@gmail.com>
2023-10-10 13:01:59 -0600

    User in Management Library System

--- java-projects-practice/src/main/java/com/libraryManagementSystem/Main.java
+++ java-projects-practice/src/main/java/com/libraryManagementSystem/Main.java
@@ -1,7 +1,19 @@
 package com.libraryManagementSystem;
 
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
+        final String[] options = { "Login", "New User" };
 
+        JOptionPane.showMessageDialog(null, "Welcome to Library Management System", "Welcome!!",
+                JOptionPane.INFORMATION_MESSAGE);
+        int result = JOptionPane.showOptionDialog(null, "Enther", "Enter", JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE, null, args, args[0]);
     }
 }
+
+// int result = JOptionPane.showOptionDialog(null, "Enter the department",
+// "Enter the department",
+// JOptionPane.YES_NO_CANCEL_OPTION,
+// JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
