9d433c32ff776890269937d20fd1e5194c1fcb87
Billy Torres <btorresr75@gmail.com>
2023-10-12 08:20:11 -0600

    feat : new user class in library management

--- java-projects-practice/src/main/java/com/libraryManagementSystem/Main.java
+++ java-projects-practice/src/main/java/com/libraryManagementSystem/Main.java
@@ -9,11 +9,36 @@ public class Main {
         JOptionPane.showMessageDialog(null, "Welcome to Library Management System", "Welcome!!",
                 JOptionPane.INFORMATION_MESSAGE);
         int result = JOptionPane.showOptionDialog(null, "Enther", "Enter", JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.INFORMATION_MESSAGE, null, args, args[0]);
-    }
-}
+                JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
 
-// int result = JOptionPane.showOptionDialog(null, "Enter the department",
-// "Enter the department",
-// JOptionPane.YES_NO_CANCEL_OPTION,
-// JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        switch (result) {
+            case 1:
+                login();
+            case 2:
+                newuser();
+            default:
+                JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private static void login() {
+        JOptionPane.showMessageDialog(null, "Enther phone number", "Phone number", 0);
+    }
+
+    private static void newuser() {
+        final String[] roles = { "Admin, Normal User" };
+
+        String name = JOptionPane.showInputDialog("Enther Name: ");
+        String phoneNumber = JOptionPane.showInputDialog("Enther Phone Number: ");
+        String email = JOptionPane.showInputDialog("Enther email: ");
+        int resultRoles = JOptionPane.showOptionDialog(null, "Roles", "Roles", JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE, null, roles, roles[0]);
+
+        if (resultRoles == 0) {
+
+        } else {
+
+        }
+    }
+
+}
