c72335d38f8c29f9273b218cd9e5d5539e5376bb
Billy Torres <btorresr75@gmail.com>
2023-10-12 10:08:20 -0600

    feat : login class and new user class

--- java-projects-practice/src/main/java/com/libraryManagementSystem/Main.java
+++ java-projects-practice/src/main/java/com/libraryManagementSystem/Main.java
@@ -3,32 +3,48 @@ package com.libraryManagementSystem;
 import javax.swing.JOptionPane;
 
 public class Main {
+    static Database database;
+
     public static void main(String[] args) {
-        Database database = new Database();
+        database = new Database();
 
-        final String[] options = { "Login", "New User" };
+        final String[] options = { "Exit", "Login", "New User" };
 
-        JOptionPane.showMessageDialog(null, "Welcome to Library Management System", "Welcome!!",
-                JOptionPane.INFORMATION_MESSAGE);
-        int result = JOptionPane.showOptionDialog(null, "Enther", "Enter", JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        int num;
+        do {
+            JOptionPane.showMessageDialog(null, "Welcome to Library Management System", "Welcome!!",
+                    JOptionPane.INFORMATION_MESSAGE);
+            int result = JOptionPane.showOptionDialog(null, "Enter", "Enter", JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
 
-        switch (result) {
-            case 1:
-                login();
-            case 2:
-                newuser();
-            default:
-                JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.INFORMATION_MESSAGE);
-        }
+            num = result;
+            switch (result) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    newuser();
+                    break;
+            }
+        } while (num != 0);
     }
 
     private static void login() {
-        JOptionPane.showMessageDialog(null, "Enther phone number", "Phone number", 0);
+        String email = JOptionPane.showInputDialog("Enther email: ");
+        String phoneNumber = JOptionPane.showInputDialog("Enther Phone Number: ");
+
+        int n = database.login(phoneNumber, email);
+        if (n != -1) {
+            User user = database.getUser(n);
+            JOptionPane.showMessageDialog(null, "Welcome " + user.getName(), "Welcomo!!",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "User doesnt exist", "User not found", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     private static void newuser() {
-        final String[] roles = { "Admin, Normal User" };
+        final String[] roles = { "Admin", "Normal User" };
 
         String name = JOptionPane.showInputDialog("Enther Name: ");
         String email = JOptionPane.showInputDialog("Enther email: ");
@@ -37,10 +53,14 @@ public class Main {
                 JOptionPane.INFORMATION_MESSAGE, null, roles, roles[0]);
 
         if (resultRoles == 0) {
-            User adming = new User(name, email, phoneNumber);
+            User admin = new User(name, email, phoneNumber);
+            database.AddUser(admin);
         } else {
             User user = new User(name, email, phoneNumber);
+            database.AddUser(user);
         }
-    }
 
+        JOptionPane.showMessageDialog(null, "User Created Succesfully", "User created",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
 }
