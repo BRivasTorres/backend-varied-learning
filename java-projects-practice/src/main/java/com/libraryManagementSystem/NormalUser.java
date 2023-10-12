a636f94993342c38a18edafe40059bac6873ccb4
Billy Torres <btorresr75@gmail.com>
2023-10-12 08:29:38 -0600

    feat : normal user class

---
+++ java-projects-practice/src/main/java/com/libraryManagementSystem/NormalUser.java
@@ -0,0 +1,11 @@
+package com.libraryManagementSystem;
+
+public class NormalUser extends User {
+    public NormalUser(String name) {
+        super(name);
+    }
+
+    public NormalUser(String name, String email, String phoneNumber) {
+        super(name, email, phoneNumber);
+    }
+}
