55bd356aac762b6057b9a42d8ea3609b8a8c3593
Billy Torres <btorresr75@gmail.com>
2023-11-28 17:04:16 -0600

    insertando datos en db con preparedStatement

--- project-crud-practice/src/main/java/com/crud/App.java
+++ project-crud-practice/src/main/java/com/crud/App.java
@@ -3,9 +3,18 @@ package com.crud;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws SQLException {
         CreateData createData = new CreateData();
-        createData.createData();
+        String nombre = JOptionPane.showInputDialog("Ingrese el titulo: ");
+        String autor = JOptionPane.showInputDialog("Ingrese el autor: ");
+        String paginasStr = JOptionPane.showInputDialog("Ingrese el número de páginas: ");
+        int paginas = Integer.parseInt(paginasStr);
+        String genero = JOptionPane.showInputDialog("Ingrese el genero: ");
+        String idioma = JOptionPane.showInputDialog("Ingrese el idioma: ");
+
+        createData.insetData(nombre, autor, paginas, genero, idioma);
     }
 }
--- project-crud-practice/src/main/java/com/crud/CreateData.java
+++ project-crud-practice/src/main/java/com/crud/CreateData.java
@@ -1,10 +1,28 @@
 package com.crud;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.swing.JOptionPane;
 
 public class CreateData {
+    public void insetData(String titulo, String autor, int paginas, String genero, String idioma) {
+        try {
+            Connection con = Database.getConnection();
+            String query = "INSERT INTO libros (titulo, autor, paginas, genero, idioma) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(query);
 
-    public void createData() {
-        JOptionPane.showInputDialog("Ingrese loas datos que desea ingresar separados por una ',' ");
+            ps.setString(1, titulo);
+            ps.setString(2, autor);
+            ps.setInt(3, paginas);
+            ps.setString(4, genero);
+            ps.setString(5, idioma);
+
+            ps.executeUpdate();
+            System.out.println("Datos añadidos");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 }
