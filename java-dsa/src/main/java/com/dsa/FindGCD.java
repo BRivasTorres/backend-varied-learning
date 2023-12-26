5be0128f0a420d8dd764e0f4b29d1aa54b7e3526
Billy Torres <btorresr75@gmail.com>
2023-12-26 09:25:03 -0600

    euclid algorithm for find the gcd number

--- java-dsa/src/main/java/com/dsa/FindGCD.java
+++ java-dsa/src/main/java/com/dsa/FindGCD.java
@@ -1,21 +1,37 @@
 public class Gcd {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int min = 0;
-
+    static  int gcd(int a, int b) {
         if(a>b) {
             min = b;
         } else {
             min = a;
         }
-
         for(int i = min; i >= 1; i--) {
             if(a%i == 0 && b%i == 0) {
                 System.out.println(i);
-                break;
+                return i;
             }
         }
+        return 1;
+    }
+
+    static euclidGcd(int a, int b) {
+        while (a != b) {
+            a>b ? a = a - b : b = b - a;
+        }
+        return a;
+    }
+
+    static euclidOptimazed(int a, int b) {
+        while(a !== 0 && b !== 0) {
+            a>b ? a = a%b : b = b%a;
+        }
+        return a == 0 ? a : b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        gcd(a, b)
     }
 }
