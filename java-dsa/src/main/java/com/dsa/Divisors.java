f397dfc85a062f21014e4e5c29fbb46f22b44c46
Billy Torres <btorresr75@gmail.com>
2024-01-04 08:48:59 -0600

    print divisors efficient aproach

--- java-dsa/src/main/java/com/dsa/Divisors.java
+++ java-dsa/src/main/java/com/dsa/Divisors.java
@@ -8,6 +8,21 @@ public class Divisors {
         }
     }
 
+    static void printDivisors(int n) {
+        int i = 0;
+        for(i = 1; i < n; i++) {
+            if(n%i == 0) {
+                System.out.println(i);
+            }
+        }
+
+        for(; i >= 1; i--) {
+            if(n%1 == 0) {
+                System.out.println(n/i);
+            }
+        }
+    }
+
     public static void main (String[] args) {
         Scanner scan = new Scanner (System.in);
         int n = scan.nextInt();
