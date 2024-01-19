f3a6052a27defc6b12b57a52a736c6af725e4d58
Billy Torres <btorresr75@gmail.com>
2024-01-18 20:11:18 -0600

    Count set bits in a number -- Java

---
+++ java-dsa/src/main/java/com/dsa/CountSetBits.java
@@ -0,0 +1,16 @@
+package com.dsa;
+
+public class CountSetBits {
+
+    static int countSetBits(int n) {
+        int count = 0;
+        while(n > 0) {
+            n = n & (n- 1);
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+    }
+}
